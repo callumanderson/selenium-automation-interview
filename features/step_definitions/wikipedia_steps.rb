6f836bb9d373be64a75e2aa6c6dc542781159d0d
root <root@callum.anderson>
2012-11-05 10:47:39 +0000

    Adding step def template

---
+++ features/step_definitions/wikipedia_steps.rb
@@ -0,0 +1,5 @@
+#You have been given the outlines of all the code you need to write
+#Some of the snippets have been written for you, you may need to
+#refactor/fix then though :)
+
+#TODO: Write step definitions
